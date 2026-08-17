525af6badbf549bb1dff855481583947852322de
Hamzahdious <hamzahdious01@gmail.com>
2026-08-17 14:28:30 +0000

    first commit

---
+++ variables.py
@@ -0,0 +1,28 @@
+# Aresmetic Oprators
+
+a = 10
+b = 3
+
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+modulos = a % b
+exponentiation = a ** b
+
+print("adition:", addition)
+print("subtraction:", subtraction)
+print("multiplication:", multiplication)
+print("division:", division)
+print("modulos:", modulos)
+print("exponentiation:", exponentiation)
+
+
+
+
+
+# Assignment Operators
+# Comparison Operators
+# Logical Operators
+# Bitwise Operators
+# Special Operators
